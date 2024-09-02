d3a2e6d19421b31963a31ccca920212189876612
g-larose <larose.g@fromtier.com>
2024-09-02 18:19:01 -0400

    added folder browser dialog

---
+++ Image Utility/Models/File.cs
@@ -0,0 +1,8 @@
+﻿namespace Image_Utility.ViewModels
+{
+    public class File
+    {
+        public string fileName { get; set; }
+        public string newFileName { get; set; }
+    }
+}
--- Image Utility/ViewModels/RenamerViewModel.cs
+++ Image Utility/ViewModels/RenamerViewModel.cs
@@ -1,15 +1,20 @@
-﻿using Image_Utility.Interfaces;
+﻿using Image_Utility.Commands;
+using Image_Utility.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace Image_Utility.ViewModels
 {
     public class RenamerViewModel: ViewModelBase
     {
         private readonly INavigator? _navigator;
+        public ICommand OpenSourceFolderCommand { get; }
 
         private List<File> _files;
         public List<File> Files
@@ -17,18 +22,51 @@ namespace Image_Utility.ViewModels
             get => _files;
             set => OnPropertyChanged(ref _files, value);
         }
+
+        private string _sourceDir;
+        public string SourceDir
+        {
+            get => _sourceDir;
+            set => OnPropertyChanged(ref _sourceDir, value);
+        }
+
+        private int _fileCount;
+        public int FileCount
+        {
+            get => _fileCount;
+            set => OnPropertyChanged(ref _fileCount, value);
+        }
         public RenamerViewModel(INavigator? navigator)
         {
             _navigator = navigator;
+            OpenSourceFolderCommand = new RelayCommand(OpenFileBrowser);
             Files = new();
             Files.Add(new File { fileName = "imageOne.jpg", newFileName = "image-001.png" });
            
         }
-    }
 
-    public class File
-    {
-        public string fileName { get; set; }
-        public string newFileName { get; set; }
+        private void OpenFileBrowser()
+        {
+            //var ofd = new OpenFileDialog();
+            //ofd.Filter = "Image files (*.jpg;*.png)|*.jpg;*.png|All files (*.*)|*.*";
+            //ofd.Multiselect = true;
+            //ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            //if(ofd.ShowDialog() == DialogResult.OK)
+            //{
+            //    SourceDir = ofd.FileName;
+            //}
+
+            var ofd = new FolderBrowserDialog();
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                SourceDir = ofd.SelectedPath;
+                var fCount = Directory.GetFiles(SourceDir).Length;
+                FileCount = fCount;
+            }
+            
+        }
     }
 }
